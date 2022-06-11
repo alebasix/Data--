2371ce5bf65faeb6ae0f3ffc5efcd2b07591bef0
OB <nuor33333333@gmail.com>
2022-06-11 12:04:37 -0400

    logic done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-!
+ const flexElement = document.getElementById("flex");
+
+const buttons = document.querySelectorAll("body>div>button");
+
+for (const button of buttons) {
+  button.addEventListener("click", (buttons) => {
+    flexElement.style.flexDirection = buttons.target.getAttribute("data-flex");
+  });
+}
